a6ce53414c4439fb4082180c948ef7c8de654fe2
laila-chk <chokri.laila.ch@gmail.com>
2023-11-13 15:17:17 +0100

    the overall home page, data is needed for finalization

--- front-end/src/pages/components/navbar.tsx
+++ front-end/src/pages/components/navbar.tsx
@@ -6,7 +6,7 @@ function NavBar() {
     <>
       <div className="absolute top-0 right-0 flex gap-1">
 
-        <div className=" mx-auto h-12 bg-purple-sh-1 rounded-full grid place-content-center overflow-hidden ">
+        <div className=" mx-auto h-12 bg-purple-sh-2 rounded-full grid place-content-center overflow-hidden ">
           <form action='' className="relative mx-auto w-max">
             <input type='search' className='w-12 h-12 bg-transparent focus:cursor-text cursor-pointer border-transparent outline-none text-transparent focus:text-impure-white relative z-10 focus:w-48 pr-8 focus:pl-4' />
 
@@ -17,14 +17,14 @@ function NavBar() {
         </div>
 
 
-        <div className=" h-12 w-12 bg-purple-sh-1 grid place-content-center  rounded-full">
+        <div className=" h-12 w-12 bg-purple-sh-2 grid place-content-center  rounded-full">
           <svg className="h-10 w-10 cursor-pointer stroke-purple stroke-[1.5px]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" >
             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
           </svg>
         </div>
 
         <Link to = "/profile">
-          <div className=" h-12 w-12 bg-purple-sh-1 grid place-content-center rounded-full">
+          <div className=" h-12 w-12 grid place-content-center ">
             <img className="h-11 w-11 rounded-full" src={pic}/>
           </div>
         </Link>
