13ed9f9d80374dbf8deff215bd16dcd6e23472e9
mohamed-souiyeh <mohamedsouiyeh3@gmail.com>
2023-11-30 18:53:37 +0100

    ignored the .env and some other files

--- back-end/src/purplerain/purplerain.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PurplerainController } from './purplerain.controller';
-import { PurplerainService } from './purplerain.service';
-
-@Module({
-  controllers: [PurplerainController],
-  providers: [PurplerainService],
-})
-export class PurplerainModule {}
--- back-end/src/purplerain/purplerain.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PurplerainService {
-  getHello(): string {
-    return 'Hello wsup! agaiiin';
-  }
-}
