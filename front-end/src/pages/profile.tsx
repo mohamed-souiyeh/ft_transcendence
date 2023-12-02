d8ffaaa247c898c376afbcd5877731d97237e4f6
laila-chk <chokri.laila.ch@gmail.com>
2023-12-02 19:18:47 +0100

    The overall user page design is done, just need data and data components for matchs history

--- front-end/src/pages/profile.tsx
+++ front-end/src/pages/profile.tsx
@@ -1,6 +1,9 @@
 import NavBar from "./components/navbar";
 import SideBar from "./components/sidebar";
 import  img  from "../assets/taha.jpg";
+import trophy from "../assets/trophy.png";
+import star from "../assets/star.png";
+import games from "../assets/controller.png"
 
 function Profile () {
   return(
@@ -9,7 +12,7 @@ function Profile () {
         <SideBar/>
         <NavBar/>
 
-        <div className="bg-purple-tone-1 bg-opacity-10 h-64 mt-32 rounded-3xl flex">
+        <div className="bg-purple bg-opacity-20 backdrop-blur-lg border-[1px] border-purple/20 h-64 mt-20 mb-16 rounded-3xl flex">
           <div className="grid place-content-center">
             <div className="bg-purple-sh-2 m-12 rounded-full h-48 w-48 grid place-content-center ">
               <img className="rounded-full h-44 w-44" src={img} />
@@ -27,37 +30,39 @@ function Profile () {
           </div>
         </div>
 
-        <div className="h-32 bg-purple-sh-2">
+        <div className="h-32 flex place-content-center mb-16">
+          <div className="bg-purple bg-opacity-10 border-[1px] border-purple/30 h-44 w-44 mx-8 mt-2 rounded-3xl grid place-content-center hover:bg-opacity-30">
+            <div className="grid place-content-center">
+              <img className="h-28 w-28 pt-2" src={star} />
+            </div>
+            <div className="grid place-content-center pt-5">
+              <p className="text-xl text-purple-tone-2 text-opacity-50">Achievs</p>
+            </div>
+          </div>
+          <div className="bg-purple bg-opacity-10 backdrop-blur-lg border-[1px] border-purple/30 h-48 w-48 mx-8 rounded-3xl grid place-content-center hover:bg-opacity-30">
+            <div className="grid place-content-center">
+              <img className="h-28 w-28 pt-2" src={trophy} />
+            </div>
+            <div className="grid place-content-center pt-9">
+              <p className="text-xl text-purple-tone-2 text-opacity-50">Wins</p>
+            </div>
+          </div>
+          <div className="bg-purple bg-opacity-10 border-[1px] border-purple/30 h-44 w-44 mx-8 mt-2 rounded-3xl grid place-content-center hover:bg-opacity-30">
+            <img className="h-28 w-28 pt-2" src={games} />
+            <div className="grid place-content-center pt-5">
+              <p className="text-xl text-purple-tone-2 text-opacity-50">Matchs</p>
+            </div>
+          </div>
         </div>
 
 
-        <div className="flex overflow-auto mt-10">
+        <div className="flex overflow-auto mt-16">
 
           <div className="grid justify-center">
             <div className="bg-purple-sh-2 h-[100%]  w-[500px] rounded-t-3xl overflow-auto ">
               <div className="sticky top-0 flex place-content-between bg-purple-sh-2 bg-opacity-70 backdrop-blur-sm rounded-t-3xl px-2 py-4 z-0" >
-                <p className="text-xl text-purple-tone-2 text-opacity-50">Matches History:</p>
+                <p className="text-xl text-purple-tone-2 text-opacity-50">Matchs History:</p>
               </div>
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
-              <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
               <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
               <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
               <p className=" text-2xl text-impure-white text-opacity-50"> Player Vs Player</p> 
@@ -73,6 +78,9 @@ function Profile () {
               <div className="sticky top-0 flex place-content-between bg-purple-sh-2 bg-opacity-70 backdrop-blur-sm rounded-t-3xl px-2 py-4 z-0" >
                 <p className="text-xl text-purple-tone-2 text-opacity-50">acheivements:</p>
               </div>
+              <p className="text-xl text-purple-tone-2 text-opacity-50">acheivement</p>
+              <p className="text-xl text-purple-tone-2 text-opacity-50">acheivement</p>
+              <p className="text-xl text-purple-tone-2 text-opacity-50">acheivement</p>
             </div>
           </div>
 
