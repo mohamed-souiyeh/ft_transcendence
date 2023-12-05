7942fa5e5d20340000f93731bb9c400d92d6f013
mohamed-souiyeh <mohamedsouiyeh3@gmail.com>
2023-12-05 16:58:39 +0100

    added env varibles to docker-compose for doppler

--- back-end/src/auth/jwt/config/jwt.config.ts
+++ back-end/src/auth/jwt/config/jwt.config.ts
@@ -9,10 +9,3 @@
 export const jwt_expire_time = {
   expiresIn: 60 * 60 * 12, // available for 12hrs
 };
-
-
-// function debug(envVariable: string): string {
-//   console.log("envVariable: ", envVariable);
-//   console.log('jwt_expire_time: ', process.env['JWT_EXPIRATON_TIME']);
-//   return envVariable;
-// }
