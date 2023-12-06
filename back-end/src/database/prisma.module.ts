2f258eda26f9d124823720c6bed884c5869a23a1
thejewels00 <jchennak@student.1337.ma>
2023-12-06 23:09:46 +0100

    prisma service and prisma module

--- back-end/src/database/prisma.module.ts
+++ back-end/src/database/prisma.module.ts
@@ -1,11 +1,24 @@
+// import { Module } from '@nestjs/common';
+// import { PrismaService } from './prisma.service';
+// import { UsersModule } from './modules/users/users.module';
+
+// @Module({
+//   providers: [PrismaService],
+//   exports: [PrismaService],
+//   imports: [UsersModule],
+// })
+
+// export class PrismaModule {}
+
+
+
+// src/prisma/prisma.module.ts
+
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { UsersModule } from './modules/users/users.module';
 
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
-  imports: [UsersModule],
 })
-
 export class PrismaModule {}
--- back-end/src/database/seed.ts
+++ back-end/src/database/seed.ts
@@ -8,14 +8,14 @@ import * as bcrypt from 'bcrypt';
 // initialize Prisma Client
 const prisma = new PrismaClient();
 
-
-
 async function main() {
   // Création d'un utilisateur
-  const user1 = await prisma.user.create({
-    data: {
-        passwordHash : await bcrypt.hash('password456', 10) , 
-        salt  : '123' , 
+  const user1 = await prisma.user.upsert({
+    where: { username: 'Alice' },
+    update: {},
+    create: {
+      passwordHash: await bcrypt.hash('password456', 10),
+      salt: '123',
       username: 'Alice',
       email: 'alice@example.com',
       // Ajoutez d'autres données d'utilisateur au besoin...
@@ -23,30 +23,40 @@ async function main() {
   });
 
   // Création d'un autre utilisateur
-  const user2 = await prisma.user.create({
-    data: {
-        passwordHash : await bcrypt.hash('passwo56', 10),
-        salt  : '123' ,
-        score : 80 ,
+  const user2 = await prisma.user.upsert({
+    where: { username: 'bob' },
+    update: {},
+    create: {
+      passwordHash: await bcrypt.hash('passwo56', 10),
+      salt: '123',
+      score: 80,
       username: 'bob',
       email: 'bob@example.com',
       // Ajoutez d'autres données d'utilisateur au besoin...
     },
   });
 
-      // Create a friendship between users
-      await prisma.users_friends.create({
-        data: {
-          userId: user1.id,
-          friendId: user2.id,
-          state: 'friends',
-        },
-      });
-
+  // Create a friendship between users
+  await prisma.users_friends.upsert({
+    where: {
+      userId_friendId: {
+        userId: user1.id,
+        friendId: user2.id,
+      },
+    },
+    update: {},
+    create: {
+      userId: user1.id,
+      friendId: user2.id,
+      state: 'friends',
+    },
+  });
 
   // Création d'une conversation avec des messages
-  const conversation = await prisma.conversation.create({
-    data: {
+  const conversation = await prisma.conversation.upsert({
+    where: { id: 1},
+    update: {},
+    create: {
       title: 'Discussion intéressante',
       participants: {
         create: [
@@ -57,7 +67,7 @@ async function main() {
       messages: {
         create: [
           { content: 'Salut, comment ça va ?', senderId: user1.id },
-          { content: 'Ça va bien, merci !', senderId: user2.id},
+          { content: 'Ça va bien, merci !', senderId: user2.id },
           // Ajoutez d'autres messages au besoin...
         ],
       },
@@ -65,8 +75,10 @@ async function main() {
   });
 
   // Création d'un match entre les deux utilisateurs
-  const match = await prisma.match.create({
-    data: {
+  const match = await prisma.match.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
       start_time: new Date(),
       end_time: new Date(),
       players: {
@@ -78,7 +90,7 @@ async function main() {
     },
   });
 
-  // Ajoutez d'autres opérations d'insertion au besoin...
+  // Ajoutez d'autres opérations d'upsert au besoin...
 
   console.log('Seed completed.');
 }
@@ -90,3 +102,84 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
+
+// async function main() {
+//   // Création d'un utilisateur
+//   const user1 = await prisma.user.create({
+//     data: {
+//         passwordHash : await bcrypt.hash('password456', 10) , 
+//         salt  : '123' , 
+//       username: 'Alice',
+//       email: 'alice@example.com',
+//       // Ajoutez d'autres données d'utilisateur au besoin...
+//     },
+//   });
+
+//   // Création d'un autre utilisateur
+//   const user2 = await prisma.user.create({
+//     data: {
+//         passwordHash : await bcrypt.hash('passwo56', 10),
+//         salt  : '123' ,
+//         score : 80 ,
+//       username: 'bob',
+//       email: 'bob@example.com',
+//       // Ajoutez d'autres données d'utilisateur au besoin...
+//     },
+//   });
+
+//       // Create a friendship between users
+//       await prisma.users_friends.create({
+//         data: {
+//           userId: user1.id,
+//           friendId: user2.id,
+//           state: 'friends',
+//         },
+//       });
+
+
+//   // Création d'une conversation avec des messages
+//   const conversation = await prisma.conversation.create({
+//     data: {
+//       title: 'Discussion intéressante',
+//       participants: {
+//         create: [
+//           { userId: user1.id },
+//           { userId: user2.id },
+//         ],
+//       },
+//       messages: {
+//         create: [
+//           { content: 'Salut, comment ça va ?', senderId: user1.id },
+//           { content: 'Ça va bien, merci !', senderId: user2.id},
+//           // Ajoutez d'autres messages au besoin...
+//         ],
+//       },
+//     },
+//   });
+
+//   // Création d'un match entre les deux utilisateurs
+//   const match = await prisma.match.create({
+//     data: {
+//       start_time: new Date(),
+//       end_time: new Date(),
+//       players: {
+//         create: [
+//           { playerId: user1.id, isWinner: true },
+//           { playerId: user2.id, isWinner: false },
+//         ],
+//       },
+//     },
+//   });
+
+//   // Ajoutez d'autres opérations d'insertion au besoin...
+
+//   console.log('Seed completed.');
+// }
+
+// main()
+//   .catch((error) => {
+//     throw error;
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
