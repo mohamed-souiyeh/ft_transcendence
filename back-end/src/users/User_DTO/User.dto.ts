9127da1fcf8722a7c7434c82ece6d7e29d98f70f
mohamed-souiyeh <mohamedsouiyeh3@gmail.com>
2023-12-15 20:12:26 +0100

    excluding file path from whoami endpoint

--- back-end/src/users/User_DTO/User.dto.ts
+++ back-end/src/users/User_DTO/User.dto.ts
@@ -5,28 +5,34 @@ import { IsEmail } from 'class-validator';
 
 export class UserDto {
   id: number | null; //
-  provider: string | null;
-  username: string | null; //
+  provider: string | null; //NOTE - needed in the database
+  
+  username: string | null; // 
 
   @Exclude()
-    profilePicture: string | null; // served
+    profilePicture: string | null; // NOTE - needed in the database
 
   //NOTE - this needs to be unique
   @IsEmail()
-  email: string | null; //
+  email: string | null; // NOTE - needed in the database and required
+
+
+  userStatus: string | null; // NOTE - needed in the database
 
   @Exclude()
   redirectUrl: string | null;
 
   //NOTE - we need to hash this for securety reasons
   @Exclude()
-  activeRefreshToken: string | null;
+  activeRefreshToken: string | null; // NOTE - needed in the database
 
   @Exclude()
-  TFAisenabled: boolean | null;
+  TFAisenabled: boolean | null; // NOTE - needed in the database
 
   @Exclude()
-  TFAsecret: string | null;
+  TFAsecret: string | null; // NOTE - needed in the database
+
+
 
   constructor(partial: Partial<UserDto>) {
     Object.assign(this, partial);
