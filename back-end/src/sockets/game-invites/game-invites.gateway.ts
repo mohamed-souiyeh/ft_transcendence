5d7007673743157c906bf534227936a08b0c0765
mohamed-souiyeh <mohamedsouiyeh3@gmail.com>
2023-12-17 11:17:18 +0100

    delegated the game invite part to mounir

--- back-end/src/sockets/game-invites/game-invites.gateway.ts
+++
@@ -1,9 +0,0 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-
-@WebSocketGateway()
-export class GameInvitesGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
-}
