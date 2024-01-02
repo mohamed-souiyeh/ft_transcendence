7bc15a3a4e18843254f5af4a59dbc271cf928dc0
laila-chk <chokri.laila.ch@gmail.com>
2024-01-02 18:19:42 +0100

    add logout functionallity

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App, { UserContext } from './App.tsx'
 import './index.css'
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
 
 axios.interceptors.response.use(
@@ -18,18 +19,32 @@ axios.interceptors.response.use(
         await axios.get("http://localhost:1337/auth/refresh", {
           withCredentials: true
         })
-          .then(() => {
-            console.log("Token refreshed ma nigga!")
-          })
-          .catch((err) => {
-            console.log("my sad shit, an err occured, it's :", err)
-          })
+        .then(() => {
+          console.log("Token refreshed ma nigga!")
+        })
+        .catch((err) => {
+          console.log("my sad shit, an err occured, it's :", err)
+        })
 
         // retrying the req
         return axios(originalRequest);
       } catch (refreshError) {
         console.error('Error refreshing token:', refreshError);
         //here, we should logout the user.
+
+        axios.get("http://localhost:1337/auth/logout", {
+          withCredentials: true
+        })
+          .then((resp) => {
+            if (resp.status == 200){
+              console.log("user successfully logged out")
+            }
+          })
+          .catch( (err) => {
+            console.log("there's nowhere to run", err)
+          })
+
+        Cookies.remove('user')
         return Promise.reject(error);
       }
     }
@@ -42,6 +57,6 @@ axios.interceptors.response.use(
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode> 
-    <App />
+  <App />
   // </React.StrictMode>
 )
