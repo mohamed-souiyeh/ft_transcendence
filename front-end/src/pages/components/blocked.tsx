3cc5439e0294db523a4a593e8ef081e6bb908216
laila-chk <chokri.laila.ch@gmail.com>
2024-01-05 18:03:37 +0100

    add Network componnents of /Chat

--- front-end/src/pages/components/blocked.tsx
+++ front-end/src/pages/components/blocked.tsx
@@ -1,7 +1,17 @@
+import img from '../../assets/taha.jpg'
 
 function Blocked() {
   return (
-    <div>Blocked</div>
+    <div className='flex border border-transparent border-b-purple-sh-0 mx-14 py-3 '>
+      <div className='flex place-items-center basis-1/2' >
+        <img src={img} className='rounded-full h-12 w-12' />
+        <p className='text-lg px-7' > SadPeepo </p>
+      </div>
+      <div className='flex flex-row-reverse  place-items-center basis-1/2' >
+         
+            <button className="rounded-lg bg-purple-sh-0 focus:outline-none border-none hover:bg-purple text-sm">Unblock</button>
+      </div>
+    </div>
   )
 }
 
