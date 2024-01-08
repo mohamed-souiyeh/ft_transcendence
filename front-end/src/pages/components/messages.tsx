947ced241d99ad4d1d05b22c6991a29fa340a513
laila-chk <chokri.laila.ch@gmail.com>
2024-01-08 20:04:14 +0100

    messages component needs submit handling and single msg design

--- front-end/src/pages/components/messages.tsx
+++ front-end/src/pages/components/messages.tsx
@@ -13,24 +13,27 @@ function Messages() {
 
 
   return (
-    <div className="bg-purple-sh-1 h-[100%] overflow-auto scrollbar-thin scrollbar-thumb-purple-sh-0 ">
+    <div className="bg-purple-sh-1 h-[100%] pb-10 ">
       <div className="flex sticky bg-purple-s-0 w-[100%] border border-transparent border-b-purple-sh-2 p-5 ">
         <div className="basis-11/12 flex">
           <img src={img} className="h-14 w-14 rounded-full" />
-        <div className="grid px-3 ">
-          <p className="text-xl font-bold text-purple-tone-2"> {user.name} </p>
-          <p className="text-impure-white/40  truncate"> status </p>
-        </div>
+          <div className="grid px-3 ">
+            <p className="text-xl font-bold text-purple-tone-2"> {user.name} </p>
+            <p className="text-impure-white/40  truncate"> status </p>
+          </div>
         </div>
         <div className="flex flex-row-reverse self-center basis-1/12" >
           <Icons/>
         </div>
       </div>
 
+      <div className="h-[87%] overflow-scroll scrollbar-thin scrollbar-thumb-purple-sh-0 ">
+        {user.message}
+      </div>
       <div className="fixed bottom-5 bg-purple-sh-0 w-[66%] h-12 m-2 rounded-lg px-2">
         <form >
           <input type='text' placeholder="send a Messages" className='w-[440px] h-12 bg-transparent outline-none rounded-lg text-impure-white px-2 place-self-center' />
-          
+
         </form >
       </div>
     </div>
