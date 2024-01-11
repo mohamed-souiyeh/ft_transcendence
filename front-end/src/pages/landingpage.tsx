b89ab0f1cfb87692ef27fc9ae032f68ec9c7c233
Aamermalika <aamermalika23@gmail.com>
2024-01-11 17:28:19 +0100

    Create the landing page has about+team+some infos

--- front-end/src/pages/landingpage.tsx
+++ front-end/src/pages/landingpage.tsx
@@ -1,7 +1,18 @@
+import Hero from "./components/Hero.tsx";
+import Team from "./components/Team.tsx";
+import Footer from "./components/Footer.tsx";
+import About from "./components/About.tsx";
+
+
 function LandingPage() {
   return (
-    <div>LandingPage</div>
-  )
+    <div className="bg-purple-sh-1 w-screen  ml-5w-screen h-screen  ">
+      <Hero />
+      <About />
+      <Team />
+      <Footer />
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
