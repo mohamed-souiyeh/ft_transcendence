66032edb28340d91ff46301bb6c0fc53f1d270f4
malika <aamermalika23@gmail.com>
2024-02-10 22:32:32 +0100

    history and acheivement pub_profile

--- front-end/src/pages/components/Acheivements.tsx
+++ front-end/src/pages/components/Acheivements.tsx
@@ -1,111 +1,45 @@
-// import newComerPic from "../../assets/newcomer.png";
-// import playerPic from "../../assets/player.png";
-// import veteranPic from "../../assets/veteran.png";
-// import axios from "axios";
-// import React, { useState, useEffect, useContext } from "react";
-
-// const [newComer, setNewComer] = useState(false);
-// const [player, setPlayer] = useState(false);
-// const [veteran, setVeteran] = useState(false);
-
-// export default function Acheivements() {
-//   return (
-//     <div className="h-[100%]  w-[350px] rounded-t-3xl overflow-hidden">
-//       <div className="bg-purple-sh-2 h-[100%]  w-[350px] rounded-t-3xl overflow-auto scrollbar-thin scrollbar-thumb-purple-sh-1">
-//         <div
-//           title="Played your first game ever"
-//           className="m-4 h-24 flex justify-start items-center border-b-2 border-purple/15"
-//         >
-//           <img
-//             src={newComerPic}
-//             className={`h-20 mx-4 ${!newComer && "grayscale-70 opacity-30"} `}
-//           />
-//           <p
-//             className={`text-2xl ${
-//               newComer ? "text-impure-white" : "text-white/30"
-//             } `}
-//           >
-//             {" "}
-//             New Comer
-//           </p>
-//         </div>
-//         <div
-//           title="Played 5 matches"
-//           className="m-4 h-24 flex justify-start items-center border-b-2 border-purple/15"
-//         >
-//           <img
-//             src={playerPic}
-//             className={`h-20 mx-4 ${!player && "grayscale-70 opacity-30"} `}
-//           />
-//           <p
-//             className={`text-2xl ${
-//               player ? "text-impure-white" : "text-white/30"
-//             } `}
-//           >
-//             {" "}
-//             Pro Player!
-//           </p>
-//         </div>
-//         <div
-//           title="Played more than 5 and won 5 matches"
-//           className="m-4 h-24 flex justify-start items-center border-b-2 border-purple/15"
-//         >
-//           <img
-//             src={veteranPic}
-//             className={`h-20 mx-4 ${!veteran && "grayscale-70 opacity-30"} `}
-//           />
-//           <p
-//             className={`text-2xl ${
-//               veteran ? "text-impure-white" : "text-white/30"
-//             } `}
-//           >
-//             {" "}
-//             Veteran!
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
-
-
 import React, { useState, useEffect } from "react";
 import newComerPic from "../../assets/newcomer.png";
 import playerPic from "../../assets/player.png";
 import veteranPic from "../../assets/veteran.png";
 import axios from "axios";
+import { useParams } from 'react-router-dom';
+
 
 
 export default function Achievements() {
   const [newComer, setNewComer] = useState(false);
   const [player, setPlayer] = useState(false);
   const [veteran, setVeteran] = useState(false);
+  const [achievs, setAchievs] = useState([]);
+  const { username } = useParams();
 
-  useEffect(() => {
- 
-    const checkAchievements = async () => {
-     
-      // const response = await axios.get(`${process.env.REACT_URL}:1337/users/Public_data/`);
-      // const achievements = response.data;
-     
-      const achievements = { playedFirstGame: true, playedFiveMatches: true, wonFiveMatches: true }; 
 
-      if (achievements.playedFirstGame) {
+
+  useEffect( () => {
+    axios.get(`${process.env.REACT_URL}:1337/users/Public_data/${username}`,
+      { withCredentials: true } )
+      .then(res => {
+        setAchievs(res.data.achievements)
+        console.log("eeee: ", res.data.achievements)
+      })
+      .catch(()=>{
+        console.log("Error getting matches history!!")
+      })
+  }, [])
+
+  useEffect (() => { 
+    achievs.forEach((achievement) => {
+      if (achievement.name === "newComer") {
         setNewComer(true);
-      }
-      if (achievements.playedFiveMatches) {
+      } else if (achievement.name === "Player") {
         setPlayer(true);
-      }
-      if (achievements.playedFiveMatches && achievements.wonFiveMatches) {
+      } else if (achievement.name === "Veteran") {
         setVeteran(true);
       }
-    };
+    });
+  },[achievs])
 
-    checkAchievements();
-  }, []); 
 
   return (
     <div className="h-[100%] w-[350px] rounded-t-3xl overflow-hidden">
--- front-end/src/pages/components/GameHistory.tsx
+++ front-end/src/pages/components/GameHistory.tsx
@@ -1,14 +1,51 @@
+import History from "./history";
+import React, { useState, useEffect } from "react";
+import newComerPic from "../../assets/newcomer.png";
+import playerPic from "../../assets/player.png";
+import veteranPic from "../../assets/veteran.png";
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+
+
+
+
+
+
+
 export default function GameHistory() {
+
+
+const [history, setHistoty] = useState([]);
+const { username } = useParams();
+
+
+  useEffect( () => {
+    axios.get(`${process.env.REACT_URL}:1337/users/Public_data/${username}`,
+      { withCredentials: true } )
+      .then(res => {
+        setHistoty(res.data.allMatches)
+        console.log("eeee: ", res.data.achievements)
+      })
+      .catch(()=>{
+        console.log("Error getting matches history!!")
+      })
+  }, [])
   return (
-      <div className="ml-10 ">
-          <p className="font-semibold text-xl tracking-wider text-purple-tone-1 pt-4 md:pt-10 ml-2 ">
-          Game History
-          </p>
-       
-        <div className="bg-purple-sh-2 border-[2px] border-purple/20 rounded-3xl h-56 w-80 ">
-          
+    <>
+    {/* <p className="font-semibold text-xl tracking-wider text-purple-tone-1 pt-4 md:pt-10 ml-2 ">
+            Achievements
+          </p> */}
+
+    <div className="flex overflow-hidden mt-16">
+      <div className="grid justify-center mx-5">
+        <div className="h-[100%]  w-[500px] rounded-t-3xl overflow-hidden">
+          <div className="bg-purple-sh-2 h-[100%]  w-[500px] rounded-t-3xl overflow-auto scrollbar-thin scrollbar-thumb-purple-sh-1 ">
+            {history.length ? history.map((matches) => <History key={matches.id}  data={matches}/>) : <p className="text-xl text-purple/50 p-5"> No history made yet </p>}
+          </div>
         </div>
       </div>
+      </div>
     
+  </>
   );
 }
